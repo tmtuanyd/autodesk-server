ab7005141b95bce5d43d3e8ebed2145ed40038d5
tmtuanyd <tmtuanyd@gmail.com>
2024-08-21 12:00:19 +0700

    update static

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const { PORT } = require("./config.js");
 const cors = require("cors");
 
 let app = express();
 app.use(express.static("wwwroot"));
+app.use(express.static(path.join(__dirname, "wwwroot")));
 app.use(cors());
 app.use(require("./routes/auth.js"));
 app.use(require("./routes/models.js"));

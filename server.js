9062b2c4832a57426d017b479e175988bfce90d9
tmtuanyd <tmtuanyd@gmail.com>
2024-08-21 10:52:06 +0700

    add server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { PORT } = require("./config.js");
+const cors = require("cors");
+
+let app = express();
+app.use(express.static("wwwroot"));
+app.use(cors());
+app.use(require("./routes/auth.js"));
+app.use(require("./routes/models.js"));
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}...`);
+});
